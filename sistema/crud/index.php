fdd003ddcde2a8648bad5d68c6f05ff5f239bacf
BrunoSqueira <47834216+BrunoSqueira@users.noreply.github.com>
2019-06-07 22:00:52 -0300

    FINALIZAÇÃO DO CRUD
    FINALIZAÇÃO DO CRUD

--- sistema/crud/index.php
+++ sistema/crud/index.php
@@ -2,28 +2,32 @@
 <html>
 
 <head>
-    <?php
+<?php
     require_once 'view/head.php';
-    ?>
+ ?>
 </head>
 
 <body>
-    <div class="container">
+<div class="container">
+
         <div class="row justify-content-center bg-light">
             <form action="" method="POST">
                 <div class="form-group">
-                    <label>Usuário</label>
-                    <input type="text" class="form-control" name="usuario" id=usuario>
+                    <label>Usúaio<label>
+                    <input type="text" class="form-control" name="usuario" id="usuario">
                 </div>
+
                 <div class="form-group">
-                    <label>Senha</label>
-                    <input type="password" class="form-control" name="senha" id=senha>
+                    <label>Senha<label>
+                    <input type="password" class="form-control" name="senha" id="senha">
                 </div>
+
 
                 <div class="form-group">
                     <button type="submit" class="btn btn-primary" name="autenticar">Entrar</button>
                     <a href="/sistema/index.html" class="btn btn-info">Voltar</a>
                 </div>
+
             </form>
         </div>
     </div>
--- sistema/crud/view/head.php
+++ sistema/crud/view/head.php
@@ -1,4 +1,4 @@
-    <meta charset="utf-8">
+<meta charset="utf-8">
     <title>Sistema CRUD</title>
     <!-- Bootstrap CSS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
